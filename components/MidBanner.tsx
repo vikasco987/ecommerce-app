feb883cf0a131157ea709ed5d7970c82fc2edc7e
vikasco987 <vikasvikas9095@gmail.com>
2025-09-04 13:33:43 +0530

    add

--- components/MidBanner.tsx
+++ components/MidBanner.tsx
@@ -4,13 +4,19 @@ import Image from "next/image";
 export default function MidBanner() {
   return (
     <div className="w-full my-8">
-      <Image
-        src="/m1.jpg" // 👈 put your banner image in public folder
-        alt="Special Offer Banner"
-        width={1200}
-        height={300}
-        className="rounded-2xl shadow-lg mx-auto"
-      />
+      <picture className="block w-full">
+        {/* Desktop / Laptop */}
+        <source srcSet="/m2.jpg" media="(min-width: 768px)" />
+        {/* Mobile */}
+        <Image
+          src="/m1.jpg"
+          alt="Special Offer Banner"
+          width={1200}
+          height={300}
+          className="rounded-2xl shadow-lg w-full h-auto mx-auto"
+          priority
+        />
+      </picture>
     </div>
   );
 }
