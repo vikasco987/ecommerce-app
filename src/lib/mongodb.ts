3b00256d44bc04dacafcc057cee9b233271e9a04
vikasco987 <vikasvikas9095@gmail.com>
2025-09-05 19:19:48 +0530

    add

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -27,18 +27,50 @@
 
 
 
+// import mongoose from "mongoose";
+
+// const MONGODB_URI =
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ecommerce";
+
+// if (!MONGODB_URI) {
+//   throw new Error("⚠️ Please define MONGODB_URI in your environment variables");
+// }
+
+// // Use a global cache to avoid multiple connections in dev/hot-reload
+// let cached = (global as any).mongoose;
+
+// if (!cached) {
+//   cached = (global as any).mongoose = { conn: null, promise: null };
+// }
+
+// export async function connectDB() {
+//   if (cached.conn) return cached.conn;
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose
+//       .connect(MONGODB_URI, {
+//         dbName: "ecommerce", // ✅ Correct spelling of your DB
+//       })
+//       .then((mongoose) => mongoose);
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+
+
+
+
+
 import mongoose from "mongoose";
 
-const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/ecommerce";
-
+const MONGODB_URI = process.env.MONGODB_URI;
 if (!MONGODB_URI) {
   throw new Error("⚠️ Please define MONGODB_URI in your environment variables");
 }
 
-// Use a global cache to avoid multiple connections in dev/hot-reload
 let cached = (global as any).mongoose;
-
 if (!cached) {
   cached = (global as any).mongoose = { conn: null, promise: null };
 }
@@ -48,12 +80,11 @@ export async function connectDB() {
 
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(MONGODB_URI, {
-        dbName: "ecommerce", // ✅ Correct spelling of your DB
-      })
+      .connect(MONGODB_URI, { dbName: "ecommerce" })
       .then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
 }
+//checkeing without fall local does it work 
